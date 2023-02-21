72846e0fa27118b137716fabfca6deee9cc3989e
squishyskull <tamara.e.brooks1@gmail.com>
2023-02-21 16:05:41 +1100

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -59,6 +59,9 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
+
+                  <label className="text-end me-1" htmlFor="cost" 
+                            style={{ marginLeft: '2rem' , size: 10}}>{currency}</label>
                     <input
                         required='required'
                         type='number'
